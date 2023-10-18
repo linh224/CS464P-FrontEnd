86fe09252758706e3ebd139ee399954ef656d5f1
linh224 <linhnguyen86904@gmail.com>
2023-10-17 22:11:59 -0700

    finished hw2

--- hw2/04-background.js
+++ hw2/04-background.js
@@ -1,1 +1,29 @@
 // Add your code here
+const randomBackground = function randomBackgroundFunction() {
+  let x = Math.ceil(Math.random() * 256);
+  let y = Math.ceil(Math.random() * 256);
+  let z = Math.ceil(Math.random() * 256);
+  let bgColor = "rgb(" + x + "," + y + "," + z + ")";
+  document.body.style.background = bgColor;
+};
+
+let intervalId;
+
+const changeBackground = function changeBackgroundFunction() {
+  let myInputValue = parseInt(document.getElementById("interval").value);
+  let button = document.getElementById("btn");
+  let buttonText = button.innerText;
+
+  if (buttonText === "Start") {
+    button.innerText = "Stop";
+    document.getElementById("btn").className = "btn btn-danger";
+    intervalId = setInterval(randomBackground, myInputValue * 1000);
+  } else {
+    button.innerText = "Start";
+    document.getElementById("btn").className = "btn btn-success";
+    clearInterval(intervalId);
+  }
+};
+
+window.addEventListener("load", changeBackground);
+document.getElementById("btn").addEventListener("click", changeBackground);
